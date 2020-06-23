d223441e6bb1a88221731d966da6ddab9e23d2d4
Jordan Juventin <33897186+2alheure@users.noreply.github.com>
2020-06-23 17:29:51 +0200

    Adding functionnalities
    Adding functionnalities such as striped rows, hovering rows, searching, passing content by prop or by slot, passing actions to set upon buttons, message when no row printed, ...

--- DataTable.vue
+++ DataTable.vue
@@ -1,51 +1,100 @@
-<style lang="postcss">
+<style lang="postcss" scoped>
+.dt > p > select,
+.dt > p > input {
+  height: 2.25rem;
+  font-size: 1rem;
+  vertical-align: middle;
+  border: 1px solid #cbd5e0;
+  border-radius: 5px;
+  padding: 0.5rem;
+}
+
+.dt table thead tr th {
+  cursor: pointer;
+}
+
+.dt table.hover tbody tr:hover {
+  background-color: #f3f3f3;
+}
+
+.dt table.stripe tbody tr:nth-of-type(even) {
+  background-color: #f6f6f6;
+}
 </style>
 
 <template>
-  <div class="container w-full md:w-4/5 xl:w-3/5 mx-auto px-2">
-    <select name="by-page" v-model="byPage">
-      <option value="10">10</option>
-      <option value="25">25</option>
-      <option value="50">50</option>
-      <option value="100">100</option>
-    </select>
+  <div class="dt">
+    <p class="flex justify-between">
+      <select name="by-page" v-model="byPage">
+        <option value="10">10</option>
+        <option value="25">25</option>
+        <option value="50">50</option>
+        <option value="100">100</option>
+      </select>
 
-    <input
-      type="search"
-      name="search"
-      class="border border-gray-400 rounded-lg"
-      v-model="searchString"
-      placeholder="Search"
-    />
+      <input type="search" name="search" v-model="searchString" placeholder="Search" />
+    </p>
 
-    <table class="stripe hover w-full py-4">
+    <table
+      :class="['w-full my-4 table-auto', (hover ? 'hover' : null), (stripe ? 'stripe' : null)]"
+    >
       <thead>
         <tr>
+          <th v-if="actions.length > 0 && !actionsAtEnd" v-html="actionsHeader"></th>
           <th
             v-for="header in headers"
-            :key="header.linkTo+'-header'"
-            v-html="header.html"
+            :key="keyPrefix+'-'+header.linkTo+'-header'"
+            :id="header.linkTo+'-header'"
+            v-html="header.html+'&nbsp;'+symbol(header.linkTo)"
             @click="changeOrderBy(header.linkTo)"
           ></th>
+          <th v-if="actions.length > 0 && actionsAtEnd" v-html="actionsHeader"></th>
         </tr>
       </thead>
-      <tbody>
-        <tr
-          v-for="(content, index) in realContents.slice((this.page - 1) * this.byPage, this.page * this.byPage)"
-          :key="index"
-        >
+
+      <tbody v-if="contents.length">
+        <tr v-for="(content, index) in realContents.slice(from-1, to)" :key="keyPrefix+'-'+index">
+          <td v-if="actions.length > 0 && !actionsAtEnd">
+            <button
+              v-for="(action, actionIndex) in actions"
+              :key="keyPrefix+'-'+index+'-button-'+actionIndex"
+              :class="action.class"
+              :style="action.style"
+              v-html="action.html"
+              @click="action.atClick(content)"
+            ></button>
+          </td>
           <td
             v-for="(header, headerIndex) in headers"
-            :key="index+'-'+headerIndex"
+            :key="keyPrefix+'-'+index+'-'+headerIndex"
+            :headers="header.linkTo"
             v-html="content[header.linkTo]"
           ></td>
+          <td v-if="actions.length > 0 && actionsAtEnd">
+            <button
+              v-for="(action, actionIndex) in actions"
+              :key="keyPrefix+'-'+index+'-button-'+actionIndex"
+              :class="action.class"
+              :style="action.style"
+              v-html="action.html"
+              @click="action.atClick(content)"
+            ></button>
+          </td>
+        </tr>
+        <tr v-if="!hasContent">
+          <td class="py-2" :colspan="headers.length + (actions.length > 0 ? 1 : 0)">{{noContentMessage}}</td>
         </tr>
       </tbody>
+
+      <tbody v-else>
+        <slot></slot>
+      </tbody>
     </table>
-    <small
-      class="block text-left py-2"
-    >Showing {{(this.page - 1) * this.byPage + 1}} to {{Math.min(this.page * this.byPage, realContents.length)}} of {{realContents.length}}.</small>
-    <Pagination v-model="page" :maxPage="Math.ceil(realContents.length / byPage)" />
+
+    <p class="flex justify-between" v-if="hasContent">
+      <small class="block text-left py-2">Showing {{from}} to {{to}} of {{maxIndex}}.</small>
+      <Pagination v-model="page" :maxPage="maxPage" />
+    </p>
   </div>
 </template>
 		
@@ -58,26 +107,119 @@ export default {
     Pagination
   },
   props: {
+    keyPrefix: {
+      // Used to prefix all keys if multiple DataTables in the same page
+      type: String,
+      default:
+        String.fromCharCode(Math.floor(Math.random() * 52 + 65)) +
+        Math.floor(Math.random() * 1000)
+    },
     headers: Array,
-    cellContents: Array
+    actions: {
+      type: Array,
+      default: () => []
+    },
+    actionsAtEnd: Boolean,
+    actionsHeader: {
+      type: String,
+      default: "Actions"
+    },
+    cellsContent: {
+      type: Array,
+      default: () => []
+    },
+    ascSymbol: {
+      type: String,
+      default: "&#8595;"
+    },
+    descSymbol: {
+      type: String,
+      default: "&#8593;"
+    },
+    neutralSymbol: {
+      type: String,
+      default: "&#8597;"
+    },
+    noContentMessage: {
+      type: String,
+      default: "Il n'y a aucune ligne à afficher."
+    },
+    hover: Boolean,
+    stripe: Boolean
   },
   data() {
     return {
-      contents: this.cellContents,
+      contents: this.cellsContent,
+      realContents: [],
+      rows: [],
       page: 1,
       byPage: 10,
       searchString: null,
-      orderBy: this.headers[0].linkTo,
+      orderBy: null,
       asc: true
     };
   },
   computed: {
-    realContents() {
-      return this.contents
+    dataByProps() {
+      return this.cellsContent.length > 0;
+    },
+    hasContent() {
+      return this.realContents.length > 0;
+    },
+    from() {
+      return (this.page - 1) * this.byPage + 1;
+    },
+    to() {
+      return Math.min(this.page * this.byPage, this.realContents.length);
+    },
+    maxIndex() {
+      return this.realContents.length;
+    },
+    maxPage() {
+      return Math.ceil(this.realContents.length / this.byPage);
+    }
+  },
+  methods: {
+    createContentArray() {
+      for (var header of this.headers) {
+        if (!header.linkTo) {
+          console.error(
+            "`headers` property objects MUST contain a `linkTo` property"
+          );
+          return;
+        }
+      }
+
+      this.contents = [];
+
+      var usrTRs = this.$el.querySelectorAll("table > tbody > tr");
+
+      itereTR: usrTRs.forEach((elem, index) => {
+        this.rows.push({ index: index, html: elem.outerHTML });
+
+        var usrTDs = this.$el.querySelectorAll(
+          "table > tbody > tr:nth-child(" + (index + 1) + ") > td"
+        );
+        if (!usrTDs.length) return;
+        var contentRow = {};
+
+        usrTDs.forEach((e, i) => {
+          contentRow[this.headers[i].linkTo] = e.innerText || null; // We must fill empty fields with null values
+        });
+        this.contents.push(contentRow);
+      });
+    },
+    updateRealContents() {
+      this.realContents = this.contents
         .map((e, i) => {
-          // Get the order by and make an array to walk through with sort
-          return { index: i, value: e[this.orderBy] };
+          // Get the orderBy and make an array to walk through with sort
+          return { index: i, value: e[this.orderBy], toStr: JSON.stringify(e) };
         })
+        .filter(e =>
+          this.searchString
+            ? e.toStr.toLowerCase().includes(this.searchString)
+            : true
+        )
         .sort((a, b) => {
           // Sort by the orderBy asc or desc
           if (this.asc) {
@@ -90,17 +232,40 @@ export default {
             else return b.value - a.value;
           }
         })
-        .map(e => this.contents[e.index]) // Get the original values
-    }
-  },
-  methods: {
+        .map(e => this.contents[e.index]);
+    },
     changeOrderBy(field) {
       if (this.orderBy == field) this.asc = !this.asc;
       else {
         this.orderBy = field;
         this.asc = true;
       }
+      this.updateRealContents();
+    },
+    symbol(header) {
+      if (header == this.orderBy) {
+        if (this.asc)
+          return '<span class="symbol">' + this.ascSymbol + "</span>";
+        else return '<span class="symbol">' + this.descSymbol + "</span>";
+      }
+
+      return '<span class="symbol">' + this.neutralSymbol + "</span>";
     }
+  },
+  watch: {
+    byPage: function(newByPage, oldByPage) {
+      // Sets the page in order to see the last rows which were printed
+      this.page = Math.ceil(((this.page - 1) * oldByPage + 1) / newByPage);
+      this.updateRealContents();
+    },
+    searchString: function(older, newer) {
+      this.page = 1;
+      this.updateRealContents();
+    }
+  },
+  mounted() {
+    if (!this.dataByProps) this.createContentArray();
+    this.updateRealContents();
   }
 };
 </script>
